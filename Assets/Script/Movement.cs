f4a4dd3f49e6822c1e1cbd7818f8e77ff0b080af
HeiXaiHuang <ziminchin129950@gmail.com>
2025-11-26 16:54:15 +0100

    IMPORTANT!!!! ANIMATION

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -3,12 +3,29 @@ using UnityEngine;
 public class BasicMovement : MonoBehaviour
 {
     public float speed = 5f;
+    public Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
+        float move_h = Input.GetAxis("Horizontal");
+        animator.SetFloat("movement", move_h * speed);
 
-        Vector3 move = new Vector3(h, 0f);
+        Vector3 move = new Vector3(move_h, 0f);
         transform.Translate(move * speed * Time.deltaTime);
+
+        if (move_h < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (move_h > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
     }
 }

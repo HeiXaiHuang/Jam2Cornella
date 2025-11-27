13771d9b224f37c1a52e580a6249e7c301768ab5
HeiXaiHuang <ziminchin129950@gmail.com>
2025-11-27 16:19:42 +0100

    Important HOME

---
+++ Assets/Script/Cleaning.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CleanAction : MonoBehaviour
+{
+    public GameObject objectToShow;
+    public SpriteRenderer spriteRenderer;
+    public Sprite cleanedSprite;
+
+    bool isPlayerInside = false;
+    public bool isClean = false;
+
+    public anxiety anxietyManager;
+    [Header("Anxiety Settings")]
+    [Range(-50f, 50f)]
+    public float anxietyAmount = 30f;
+
+    void Start()
+    {
+        objectToShow.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (isPlayerInside && !isClean && Input.GetKeyDown(KeyCode.E))
+        {
+            objectToShow.SetActive(true);
+            spriteRenderer.sprite = cleanedSprite;
+            isClean = true;
+
+            // Añadir ansiedad configurable al limpiar
+            if (anxietyManager != null)
+            {
+                anxietyManager.AddAnxiety(-anxietyAmount);
+                Debug.Log("Anxiety modificado en: " + anxietyAmount);
+            }
+
+            StartCoroutine(HideObjectAfterDelay(3f));
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            isPlayerInside = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            isPlayerInside = false;
+    }
+
+    IEnumerator HideObjectAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        objectToShow.SetActive(false);
+    }
+}
--- Assets/Script/InteractCollider.cs
+++ Assets/Script/InteractCollider.cs
@@ -1,6 +1,19 @@
 using UnityEngine;
 
-public class InteractCollider
+public class TagTrigger : MonoBehaviour
 {
-    
+    public string targetTag;
+    public GameObject targetObject;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(targetTag))
+            targetObject.SetActive(true);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(targetTag))
+            targetObject.SetActive(false);
+    }
 }

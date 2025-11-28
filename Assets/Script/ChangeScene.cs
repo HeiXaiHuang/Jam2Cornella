a0b95fe0d52029324e699820ca264d6072e8ef5a
HeiXaiHuang <ziminchin129950@gmail.com>
2025-11-28 10:57:38 +0100

    Change

--- Assets/Script/ChangeScene.cs
+++ Assets/Script/ChangeScene.cs
@@ -3,10 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    public string sceneName;
+    public string sceneName; // Nombre de la escena a cargar
 
+    // M¨¦todo normal: se puede llamar desde un bot¨®n o evento
     public void ChangeScene()
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    // M¨¦todo autom¨¢tico: se ejecuta al entrar un objeto con tag "Player" en el trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
     }
 }
